490a2b7e097dd7ad95dd3daf675fb5e5bdab0d21
manojbandi64 <manojbandi@live.com>
2022-07-29 20:24:17 +0530

    setup done

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'src',
+    version="0.0.1",
+    description="Its a wine Q package",
+    author="Manoj",
+    packages=find_packages(),
+    license="MIT"
+)
